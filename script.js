4cd08522bbe2555d62617cb6f3aa1895ed00593b
kausik <kkausik11@gmail.com>
2024-01-14 00:53:44 +0530

    Moderate UI and add song to the list

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+async function getsong(){
+    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let res  = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = res;
+    let songs = [];
+    let song = div.getElementsByTagName("a");
+    for (let index = 0; index < song.length; index++) {
+        const element = song[index];
+       if(element.href.endsWith(".mp3")){
+        songs.push(element.href);
+
+       }
+        
+    }
+   return songs;
+    
+};
+
+async function show(){
+
+    let songs = await getsong();
+    console.log(songs);
+
+ let lib = document.querySelector(".songList").getElementsByTagName('ul')[0];
+ console.log(lib)
+  for (const song of songs) {
+    lib.innerHTML = lib.innerHTML + `<li>${song}</li>`;
+    console.log(song)
+    
+  }
+
+    let audio = new Audio(songs[1]);
+    audio.play();
+}
+show();
+
+
+
